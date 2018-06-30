e887709d6ff8d103209bd00b58ce528482a34741
lmvmom <38746404+lmvmom@users.noreply.github.com>
2018-06-30 00:49:05 -0700

    week 8
    progress on storm center

--- assignments/lesson-8/scripts/currentdate.js
+++ assignments/lesson-8/scripts/currentdate.js
@@ -1,2 +1,6 @@
 var d=new Date();
 document.getElementById('currentdate').innerHTML = d.toDateString();
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
